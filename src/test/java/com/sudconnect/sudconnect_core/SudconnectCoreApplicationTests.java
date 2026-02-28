0be072f35c20814df41c342fd5103466fc643abc
sazarenko <sergii_azarenko@epam.com>
2026-02-28 13:16:05 -0800

    SUDC-0001 created skeleton

---
+++ src/test/java/com/sudconnect/sudconnect_core/SudconnectCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudconnect.sudconnect_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudconnectCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
